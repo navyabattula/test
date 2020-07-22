d8305194a3861edf03fa71edd245f7601d92df55
pavanichunduri <navyabio12@gmail.com>
2020-07-22 15:20:08 +0530

    First Commit

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.vogella.maven.quick__start;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
